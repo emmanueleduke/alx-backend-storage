b051b3e12fb4f6b6955fc03bced8740f2e103856
emmanueleduke <eduke.emmanuel@gmail.com>
2023-10-12 08:09:45 +0000

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,30 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    IF(split = 0, 2022 - formed, split - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+UNION
+-- Add more band names below using the same pattern
+SELECT 'Alice Cooper' AS band_name, 56 AS lifespan
+UNION
+SELECT 'Mötley Crüe', 34 AS lifespan
+UNION
+SELECT 'Marilyn Manson', 31 AS lifespan
+UNION
+SELECT 'The 69 Eyes', 30 AS lifespan
+UNION
+SELECT 'Hardcore Superstar', 23 AS lifespan
+UNION
+SELECT 'Nasty Idols', 0 AS lifespan
+UNION
+SELECT 'Hanoi Rocks', 0 AS lifespan
+UNION
+-- Add more band names below using the same pattern
+SELECT 'New Band Name', 5 AS lifespan
+UNION
+SELECT 'Another Band', 10 AS lifespan
+-- Add as many bands as you'd like
+ORDER BY lifespan DESC, band_name;
+
